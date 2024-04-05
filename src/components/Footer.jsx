8d9461202743532394949261821e4163704bad06
thejyotipatel <jyotipatelfyit77100@gmail.com>
2024-04-05 08:43:54 -0700

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,147 +1,198 @@
 const Footer = () => {
   return (
-    <div className='container-lg'>
-      {/* <footer className='py-5'>
-        <div className='row'>
-          <div className='col-6 col-md-2 mb-3'>
-            <ul className='nev flex-column'>
-              <li className='nav-item mb-2'>
-                <h5 className=''>Hobbycue Quick Links</h5>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  About Us
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Our Services
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Work with Us
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  FAQ
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className='col-6 col-md-2 mb-3'>
-            <ul className='nev flex-column'>
-              <li className='nav-item mb-2'>
-                <h5 className=''>How Do I</h3>
-              </l5>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Sign Up
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Add a Listing
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Claim Listing
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Post a Query
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Add a Blog Post
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Other Queries
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className='col-6 col-md-2 mb-3'>
-            <ul className='nev flex-column'>
-              <li className='nav-item mb-2'>
-                <h5 className=''>Quick Links</h3>
-              </l5>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Listings
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Blog Posts
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Shop / Store
-                </a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'>
-                  Community
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className='col-md-5 offset-md-1 mb-3'>
-            <h5 className=''>Social Media</h35
+    <>
+      <div className='container-lg'>
+        <footer className='py-5'>
+          <div className='row'>
+            <div className='col-3 col-md-2 mb-3'>
+              <ul role='list' className='nev flex-column p-0'>
+                <li className='nav-item mb-3'>
+                  <h6>Hobbycue</h6>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link  text-body-secondary  '>
+                    About Us
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary  '>
+                    Our Services
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Work with Us
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    FAQ
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-            <ul className='nev flex-column'>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-              <li className='nav-item mb-2'>
-                <a href='' className='nav-link p-0 text-body-secondary'></a>
-              </li>
-            </ul>
+            <div className='col-3 col-md-2 mb-3'>
+              <ul role='list' className='nev flex-column p-0'>
+                <li className='nav-item mb-3'>
+                  <h6>How Do I</h6>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Sign Up
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Add a Listing
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Claim Listing
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Post a Query
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Add a Blog Post
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Other Queries
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-            <h5 className=''>Invite Friends</h5>
-            <inp5t
-              type='email'
-              className='form-control'
-              id='email'
-              placeholder='Email ID'
-            /> 
-            <button className="btn">Invite</button>
+            <div className='col-3 col-md-2 mb-3'>
+              <ul role='list' className='nev flex-column p-0'>
+                <li className='nav-item mb-3'>
+                  <h6>Quick Links</h6>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Listings
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Blog Posts
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Shop / Store
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a href='' className='nav-link text-body-secondary'>
+                    Community
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className='col-md-5 offset-md-1 mb-3'>
+              <h6>Social Media</h6>
+
+              <ul
+                role='list'
+                className='nev d-flex gap-3 flex-row p-0 mt-3 mb-5 '
+              >
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-facebook '></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-twitter-x'></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-instagram'></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-pinterest'></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-google'></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-youtube'></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-telegram'></i>
+                  </a>
+                </li>
+                <li className='nav-item mb-2 d-flex'>
+                  <a
+                    href=''
+                    className='nav-link text-body-secondary px-2 py-1 bg-body-secondary rounded-circle'
+                  >
+                    <i class='bi bi-envelope-fill'></i>
+                  </a>
+                </li>
+              </ul>
+
+              <h5>Invite Friends</h5>
+              <inp5t
+                type='email'
+                className='form-control'
+                id='email'
+                placeholder='Email ID'
+              />
+              <button className='btn'>Invite</button>
+            </div>
           </div>
-        </div>
+        </footer>
+      </div>
 
-        <div className='d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top'>
-          <p>© Purple Cues Private Limited</p>
-        </div>
-      </footer> */}
-    </div>
+      <div className='d-flex  justify-content-center py-4    bg-opacity-10  bg-secondary'>
+        <h6>© Purple Cues Private Limited</h6>
+      </div>
+    </>
   )
 }
 export default Footer
--- src/components/communityComponent.jsx
+++ src/components/communityComponent.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 const CommunityComponent = () => {
+  const [showPopover, setShowPopover] = useState(false)
+
   return (
     <>
       <div className='bg-body-tertiary bg-secondary hero-component'>
@@ -11,59 +15,19 @@ const CommunityComponent = () => {
             <div className='d-inline'>
               <button className='btn btn-purple'>Get started</button>
             </div>
+          </div>
 
-            <div className='me-auto'>
-              <button
-                type='button'
-                class='btn btn-secondary'
-                data-bs-toggle='tooltip'
-                data-bs-placement='bottom'
-                data-bs-title='Tooltip on bottom'
-              >
-                Tooltip on bottom
-              </button>
-              <p className='p-2'>
-                <i class='bi bi-chevron-up text-purple'></i>
-              </p>
-              <p className='muted'>
-                Placeholder text to demonstrate some{' '}
-                <a
-                  href='#'
-                  data-bs-toggle='tooltip'
-                  data-bs-title='Default tooltip'
-                >
-                  inline links
-                </a>{' '}
-                with tooltips. This is now just filler, no killer. Content
-                placed here just to mimic the presence of{' '}
-                <a
-                  href='#'
-                  data-bs-toggle='tooltip'
-                  data-bs-title='Another tooltip'
-                >
-                  real text
-                </a>
-                . And all that just to give you an idea of how tooltips would
-                look when used in real-world situations. So hopefully you've now
-                seen how{' '}
-                <a
-                  href='#'
-                  data-bs-toggle='tooltip'
-                  data-bs-title='Another one here too'
-                >
-                  these tooltips on links
-                </a>{' '}
-                can work in practice, once you use them on{' '}
-                <a
-                  href='#'
-                  data-bs-toggle='tooltip'
-                  data-bs-title='The last tip!'
-                >
-                  your own
-                </a>{' '}
-                site or project.
-              </p>
-            </div>
+          <div className='  d-inline-flex container-lg  mt-5 pt-5 gap-4  hero-bg-img1'>
+            <img
+              src='images/community01.svg'
+              alt='community'
+              className=' col  img-fluid'
+            />
+            <img
+              src='images/community02.png'
+              alt='community'
+              className='col img-fluid'
+            />
           </div>
         </div>
       </div>
